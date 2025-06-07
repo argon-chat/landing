8e2a271a8410c8f4d1ebe50222ef78c0125b85af
Yuuki Wesp <ls-micro@ya.ru>
2025-06-08 01:11:29 +0300

    add robots txt, add site to sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://argon.gl",
   integrations: [vue(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
